e2f91a38a4d673e3652329300e3b5b36917027ff
Victor Barbosa <126208560+VictorAuBarbosa@users.noreply.github.com>
2024-03-01 21:46:37 -0300

    Update settings.py

--- FirstDjango/FirstDjango/settings.py
+++ FirstDjango/FirstDjango/settings.py
@@ -25,13 +25,7 @@ SECRET_KEY = 'django-insecure-ol8$0a26a^(h%$rykm3c2wy!ylkjdg#2oaw=atws!de9l4wzgs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-# Configuração de produção
-else:
-    ALLOWED_HOSTS = ['VictorAB.pythonanywhere.com', 'arvomartins01.pythonanywhere.com', 'gabriefalc1.pythonanywhere.com']
-
+ALLOWED_HOSTS = ["gabriefalc1.pythonanywhere.com/"]
 
 # Application definition
 
