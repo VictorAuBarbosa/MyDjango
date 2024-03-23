42a2662a17db67b2281653ced53d08905bd74fca
VictorAuBarbosa <babogivi@gmail.com>
2024-03-22 21:37:39 -0300

    5

--- FirstDjango/FirstDjango/settings.py
+++ FirstDjango/FirstDjango/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / "mycontacts/static",
+    os.path.join(BASE_DIR, 'static'),
 ]
